bdf024c2e7b93a7a7ddb733532b84b123f085225
moz <moz>
2000-08-18 01:20:12 +0000

    Typo fixes.

--- dae/opd_proc.c
+++ dae/opd_proc.c
@@ -1006,8 +1006,8 @@ void opd_handle_mapping(const struct op_sample *sample)
 
 	/* eip is actually nr. of bytes to read from map device */
 	size = (ssize_t)sample->eip;
-	if (size<sizeof(u32)*4) {
-		fprintf(stderr,"oprofiled: size is less than 32 : %lu\n", size);
+	if (size<(ssize_t)sizeof(u32)*4) {
+		fprintf(stderr,"oprofiled: size is less than 32 : %u\n", size);
 		exit(1);
 	}
 
@@ -1016,7 +1016,7 @@ void opd_handle_mapping(const struct op_sample *sample)
 
 	opd_read_device(mapdevfd,buf,size,0);
 
-	while (pos < size/(sizeof u32)) { 
+	while (pos < size/sizeof(u32)) { 
 		mapping = (struct op_mapping *)&buf[pos];
 		pos += 4;
  
