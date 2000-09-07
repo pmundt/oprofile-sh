6228b4653afb47c8170cc930335536608f9f1b57
moz <moz>
2000-09-07 20:12:47 +0000

    Use oprof_ready rather than global variable mapbuf_wakeup.
    Use -EINTR not -EAGAIN
    
    Increase size of map buffer.
    
    Increase OP_MAP_BUF_WATERMARK.
    
    Allocate map buffer with vmalloc().
    
    Wake up daemon every OP_MAP_BUF_WATERMARK entries in map buffer.

--- oprofile.h
+++ oprofile.h
@@ -76,13 +76,13 @@ struct _oprof_data {
 #define OP_EXIT ((1U<<15)|(1U<<4))
 
 /* size of map buffer in u32 */
-#define OP_MAX_MAP_BUF 32768
+#define OP_MAX_MAP_BUF 262144
 
-/* oprof_data->ready will be set this many entries
- * in the map device before the end, to avoid overflow
- * in the case where the daemon hasn't read anything yet
+/* oprof_data->ready will be set every time this many
+ * entries are added to the map buffer in an attempt to
+ * prevent overflow
  */
-#define OP_MAP_BUF_WATERMARK 256
+#define OP_MAP_BUF_WATERMARK 1024
 
 /* nr. entries in hash map, prime */
 #define OP_HASH_MAP_NR 1023
