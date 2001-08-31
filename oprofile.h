0c6b8ea5e61361a713e1f15cf9af5385dc9ae71d
movement <movement>
2001-08-31 17:16:35 +0000

    mptable parse

--- oprofile.h
+++ oprofile.h
@@ -32,6 +32,9 @@
 /* userspace/module interface */
 #include "op_user.h"
 
+#undef min
+#undef max
+ 
 #define OP_NR_ENTRY (SMP_CACHE_BYTES/sizeof(struct op_sample))
 
 struct op_entry {
@@ -226,6 +229,7 @@ extern uint ctrreg2;
 extern uint ctrreg3;
 
 int oprof_init(void);
+int find_intel_smp(void);
 void oprof_exit(void);
 void my_set_fixmap(void);
 int op_min_count(u8 ctr_type);
