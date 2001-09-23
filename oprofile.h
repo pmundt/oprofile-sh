08773c0d8a05ea4f500afbca6ef72accf86bcc14
movement <movement>
2001-09-23 16:15:51 +0000

    2.4.10 support.
    Note: oprofpp -l vmlinux is very broken at the moment !
    gprof output looks OK though.

--- oprofile.h
+++ oprofile.h
@@ -216,6 +216,13 @@ struct _idt_descr { u32 a; u32 b; } __attribute__((__packed__));
 #include <linux/completion.h>
 #endif
 
+// 2.4.10 introduced APIC setup under normal APIC config
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,4,10)
+#ifndef CONFIG_X86_UP_APIC
+#define NEED_FIXMAP_HACK
+#endif
+#endif
+ 
 // 2.4.10 introduced MODULE_LICENSE
 #ifndef MODULE_LICENSE
 #define MODULE_LICENSE(x)
